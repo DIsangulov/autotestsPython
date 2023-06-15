665002fa1a01df3c9d899669a592670c2301e4e1
Is Denis <d.isangulov@bftcom.com>
2023-06-15 11:41:35 +0300

    QA-164 test add

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -2,7 +2,8 @@ import os
 
 import allure
 
-from pages.UI.Auth.auth_page import AuthPage
+from pages.UI._0_Auth.auth_page import AuthPage
+from pages.UI._12_Logging.session_log import SessionLog
 
 # ________ constants __________
 # region
@@ -23,13 +24,42 @@ class TestAuth:
         with allure.step("Проверяем что авторизация прошла успешно"):
             step.should_enter_be_successful()
 
-    @allure.title("Авторизация, позитивный кейс")
+    @allure.title("Выход из системы")
     def test_logout(self, browser):
         step = AuthPage(browser, link)
         with allure.step("Нажимаем на кнопку Выход"):
             step.log_out()
         with allure.step("Выход произведен успешно"):
             step.shold_log_out_be_successful()
+
+
+@allure.suite("Сценарные проверки по ПиМИ")
+class TestScenariosPimi:
+    @allure.title("Проверка показа деталей")
+    def test_checking_details(self, browser):
+        step = AuthPage(browser, link)
+        with allure.step("Заходим на тестовый стенд"):
+            step.open()
+        with allure.step("Вводим корректные логин и пароль"):
+            step.enter_as_user()
+        step = SessionLog(browser, link)
+        with allure.step("Переходим в Логирование -> Лог сессии"):
+            step.open_session_log()
+        with allure.step("Вводим дату начала поиска 01.01.2023"):
+            step.enter_date_data("01.01.2023")
+        with allure.step("Нажимаем на кнопку Поиск"):
+            step.click_search()
+        with allure.step("Нажимаем на Опции -> Показать детали"):
+            step.click_sessions_details()
+        with allure.step("Проверяем что Детали команды содержат информацию"):
+            step.should_session_log_be_successful()
+
+
+
+
+
+
+
 
 
 
