48f1e4b15fed5fb1e431fd401106b5d4bf65a265
Is Denis <d.isangulov@bftcom.com>
2023-07-12 11:57:26 +0300

    all added tests by 12_07_23

--- tests/test_ui.py
+++ tests/test_ui.py
@@ -9,8 +9,10 @@ from pages.UI._3_Administration.system_config_man import SystemConfigMan
 from pages.UI._6_Policy_Control.portal_functions import PortalFunctions
 from pages.UI._6_Policy_Control.session_policy import SessionPolicy
 from pages.UI._8_User_Management.users_page import UserDefinition
+from pages.UI._8_User_Management.user_groups_page import UserGroupDefinition
 from pages.UI._12_Logging.session_log import SessionLog
 from pages.UI._2_Device_Management.device_groups import DeviceGroups
+
 
 # ________ constants __________
 # region
@@ -20,7 +22,7 @@ link = os.environ.get('TARGET_URL', "https://10.0.5.27")
 # endregion
 # ________ constants __________
 
-
+@pytest.mark.skip
 @allure.suite("Проверка доступности веб-интерфейса")
 class TestAuth:
     @allure.title("Авторизация, позитивный кейс")
@@ -48,6 +50,77 @@ class TestAuth:
             step.shold_log_out_be_successful()
 
 
+@pytest.mark.skip
+@allure.suite("Страница 'Определение пользователя'")
+class TestUser:
+    @allure.title("Создание пользователя, позитивный кейс")
+    def test_create_user(self, browser):
+        step = AuthPage(browser, link)
+        with allure.step("Заходим на тестовый стенд"):
+            step.open()
+        with allure.step("Вводим корректные логин и пароль"):
+            step.enter_as_user()
+        step = UserDefinition(browser, link)
+        with allure.step("Переходим на страницу 'Определение пользователя'"):
+            step.open_user_definition()
+        with allure.step("Вводим корректные данные"):
+            step.enter_user("autotest_user", "1", "qwerty", "qwerty", "autotest", "autotest", "autotest@test.com",
+                        "89999999999")
+        with allure.step("Нажимаем сохранить"):
+            step.click_save()
+        with allure.step("Поиск созданного пользователя"):
+            step.search_user("autotest_user")
+        with allure.step("Проверяем, что пользователь создан успешно"):
+            step.should_create_user_be_successful("autotest_user")
+
+
+@pytest.mark.skip
+@allure.suite("Страница 'Определение группы пользователей'")
+class TestUserGroup:
+    @allure.title("Создание группы пользователей, позитивный кейс")
+    def test_create_user_group(self, browser):
+        step = AuthPage(browser, link)
+        with allure.step("Заходим на тестовый стенд"):
+            step.open()
+        with allure.step("Вводим корректные логин и пароль"):
+            step.enter_as_user()
+        step = UserGroupDefinition(browser, link)
+        with allure.step("Переходим на страницу 'Определение группы пользователей'"):
+            step.open_user_group_definition()
+        with allure.step("Вводим корректные данные"):
+            step.enter_user_group("at_gp", "autotest_user", "at")
+        with allure.step("Нажимаем сохранить"):
+            step.click_save()
+        with allure.step("Нажимаем подтвердить"):
+            step.click_confirm()
+        with allure.step("Поиск созданной группы"):
+            step.search_user_group("at_gp")
+        with allure.step("Проверяем, что группа создана успешно"):
+            step.should_create_user_group_be_successful("at_gp")
+
+
+@pytest.mark.skip
+@allure.suite("Страница 'Определение пользователя'")
+class TestDeleteUser:
+    @allure.title("Удаление пользователя, позитивный кейс")
+    def test_delete_user(self, browser):
+        step = AuthPage(browser, link)
+        with allure.step("Заходим на тестовый стенд"):
+            step.open()
+        with allure.step("Вводим корректные логин и пароль"):
+            step.enter_as_user()
+        step = UserDefinition(browser, link)
+        with allure.step("Переходим на страницу 'Определение пользователя'"):
+            step.open_user_definition()
+        with allure.step("Поиск пользователя"):
+            step.search_user("autotest_user")
+        with allure.step("Удаляем пользователя"):
+            step.delete_user()
+        with allure.step("Проверяем, что пользователь удален"):
+            step.should_delete_user_be_successful("autotest_user")
+
+
+@pytest.mark.skip
 @allure.suite("Сценарные проверки по ПиМИ")
 class TestScenariosPimi:
     @allure.title("Проверка показа деталей")
@@ -89,6 +162,7 @@ class TestScenariosPimi:
             step.should_play_session_run()
 
 
+@pytest.mark.skip
 @allure.suite("Настройка политик устройств")
 class TestConfiguringDevicePolicies:
     @allure.title("Группы устройств - Создание группы устройств")
