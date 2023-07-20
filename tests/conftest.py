0499a612bcae7d01482f23a863b654fac2e329c4
Is Denis <d.isangulov@bftcom.com>
2023-07-20 16:54:14 +0300

    complex tests add

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,27 +5,27 @@ import pytest
 from playwright.sync_api import sync_playwright
 
 
-@pytest.fixture(scope='function')
-def browser():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(channel="chrome", headless=False)
-        context = browser.new_context(ignore_https_errors=True, viewport={"width": 1920, "height": 1080})
-        page = context.new_page()
-        yield page
-        page.close()
-        browser.close()
-
-#
 # @pytest.fixture(scope='function')
 # def browser():
 #     with sync_playwright() as playwright:
-#         browser = playwright.chromium.launch(channel="chrome", headless=True)
+#         browser = playwright.chromium.launch(channel="chrome", headless=False)
 #         context = browser.new_context(ignore_https_errors=True, viewport={"width": 1920, "height": 1080})
 #         page = context.new_page()
 #         yield page
-#         take_screenshot(page)
 #         page.close()
 #         browser.close()
+
+
+@pytest.fixture(scope='function')
+def browser():
+    with sync_playwright() as playwright:
+        browser = playwright.chromium.launch(channel="chrome", headless=True)
+        context = browser.new_context(ignore_https_errors=True, viewport={"width": 1920, "height": 1080})
+        page = context.new_page()
+        yield page
+        take_screenshot(page)
+        page.close()
+        browser.close()
 
 
 def take_screenshot(page):
