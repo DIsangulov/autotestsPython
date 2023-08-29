37ca21dbbbef9253fb85c93b6d7d3acf4b699eca
Is Denis <d.isangulov@bftcom.com>
2023-08-29 11:21:20 +0300

    QA-207 test added

--- pages/UI/_6_Policy_Control/active_sessions.py
+++ pages/UI/_6_Policy_Control/active_sessions.py
@@ -16,3 +16,13 @@ class ActiveSessions(BasePage):
         self.page.frame_locator(MainLocators.MAIN_FRAME).nth(2).get_by_text("Показать детали").click()
         time.sleep(2)
         assert self.page.frame_locator(MainLocators.MAIN_FRAME).nth(2).locator("//*[text()='pwd']").is_visible()
+
+    def play_active_SSH_session(self):
+        self.page.frame_locator(MainLocators.MAIN_FRAME).nth(2).get_by_text("Поиск").nth(1).click()
+        self.page.frame_locator(MainLocators.MAIN_FRAME).nth(2).locator("(//div[contains(text(), 'SSH')]/preceding::button[1])[2]").click()
+        self.page.frame_locator(MainLocators.MAIN_FRAME).nth(2).get_by_text("Воспроизвести сессию").click()
+        time.sleep(5)
+        with self.page.context.expect_page() as window:
+            new_window = window.value
+            assert new_window.locator("#terminal-container").is_visible()
+
