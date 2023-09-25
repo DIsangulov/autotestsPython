b818810082e8bcbdcca27f9bbea5528d1931b632
Is Denis <d.isangulov@bftcom.com>
2023-09-25 10:16:39 +0300

    base configuration done (some changes)

--- pages/UI/_8_User_Management/assigned_credential.py
+++ pages/UI/_8_User_Management/assigned_credential.py
@@ -1,3 +1,5 @@
+import time
+
 from pages.Helpers.base_page import BasePage
 from resources.locators import MainLocators
 
@@ -9,13 +11,14 @@ class AssignedCredential(BasePage):
         self.page.get_by_text("Назначенные аккаунты").click()
 
     def assigning_user_rights(self, user_name):
-        self.page.frame_locator(MainLocators.MAIN_FRAME).locator("//label[contains(text(), 'Выбор пользователя')]/following::input[1]").click()
+        self.page.frame_locator(MainLocators.MAIN_FRAME).locator("//label[contains(text(), 'Выбор пользователя:')]/following::input[1]").click()
         self.page.frame_locator(MainLocators.MAIN_FRAME).locator("//*[text()='User']").click()
-        self.page.frame_locator(MainLocators.MAIN_FRAME).locator("//label[contains(text(), 'Источник назначения')]/following::input[1]").click()
+        self.page.frame_locator(MainLocators.MAIN_FRAME).locator("//label[contains(text(), 'Источник назначения:')]/following::input[1]").click()
         self.page.frame_locator(MainLocators.MAIN_FRAME).locator("//div[@title='SAPM']").click()
-        self.input_text_field("Имя пользователя:", user_name)
-        self.delay_input(MainLocators.MAIN_FRAME, "//label[contains(text(), 'Имя пользователя SAPM:')]/following::input[1]", "pamuser")
-        self.page.frame_locator(MainLocators.MAIN_FRAME).locator("//p[contains(text(), 'pamuser')]").click()
+        self.delay_input(MainLocators.MAIN_FRAME, "//label[contains(text(), 'Имя пользователя:')]/following::input[1]", "admin")
+        self.page.frame_locator(MainLocators.MAIN_FRAME).locator("//p[contains(text(), 'Username: admin')]").click()
+        self.delay_input(MainLocators.MAIN_FRAME, "//label[contains(text(), 'Имя пользователя SAPM:')]/following::input[1]", user_name)
+        self.page.frame_locator(MainLocators.MAIN_FRAME).locator("//p[contains(text(), '"+str(user_name)+"')]").click()
         self.page.frame_locator(MainLocators.MAIN_FRAME).locator("//*[text()='Сохранить']").click()
         self.page.frame_locator(MainLocators.MAIN_FRAME).locator("//*[text()='Да']").click()
 
